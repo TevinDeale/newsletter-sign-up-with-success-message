f84ff5c005e821e05fd90c6cc4c328108491fa5c
Tevin Deale <103233487+TevinDeale@users.noreply.github.com>
2023-11-12 23:41:44 -0600

    styled the right side

--- newsletter-sign-up-app/src/App.jsx
+++ newsletter-sign-up-app/src/App.jsx
@@ -4,28 +4,29 @@ import './App.css'
 function App() {
 
   return (
-    <div className='app-container'>
-      <main className='left'>
-        <div className='left-content'>
-          <h1>Stay updated!</h1>
-          <p>Join 60,000+ product managers receiving monthly updates on:</p>
-          <ul>
-            <li>Product discovery and building wht matters</li>
-            <li>Measuring to ensure updates are a success</li>
-            <li>And much more!</li>
-          </ul>
-          <form>
-            <h4>Email address</h4>
-            <input type="email" placeholder='email@company.com' />
-            <br />
-            <button>Subscribe to monthly newsletter</button>
-          </form>
-        </div>
-      </main>
-
-      <main className='right'>
-        <img src='../src/assets/images/illustration-sign-up-desktop.svg' alt='illustration' />
-      </main>
+    <div className="container">
+      <div className='app-container'>
+        <main className='left'>
+          <div className='left-content'>
+            <h1>Stay updated!</h1>
+            <p>Join 60,000+ product managers receiving monthly updates on:</p>
+            <ul>
+              <li>Product discovery and building wht matters</li>
+              <li>Measuring to ensure updates are a success</li>
+              <li>And much more!</li>
+            </ul>
+            <form>
+              <h4>Email address</h4>
+              <input type="email" placeholder='email@company.com' />
+              <br />
+              <button>Subscribe to monthly newsletter</button>
+            </form>
+          </div>
+        </main>
+        <main className='right'>
+          <img src='../src/assets/images/illustration-sign-up-desktop.svg' alt='illustration' />
+        </main>
+      </div>
     </div>
   )
 }
