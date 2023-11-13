ddfb277f5772b30eab41266fc3dc8900ec3ed97e
Tevin Deale <103233487+TevinDeale@users.noreply.github.com>
2023-11-13 00:23:51 -0600

    completed styling the left

--- newsletter-sign-up-app/src/App.jsx
+++ newsletter-sign-up-app/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
             <h1>Stay updated!</h1>
             <p>Join 60,000+ product managers receiving monthly updates on:</p>
             <ul>
-              <li>Product discovery and building wht matters</li>
+              <li>Product discovery and building what matters</li>
               <li>Measuring to ensure updates are a success</li>
               <li>And much more!</li>
             </ul>
