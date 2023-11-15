ba2262faec482ec5e0df99fb6ffec1ab09f35f13
Tevin Deale <103233487+TevinDeale@users.noreply.github.com>
2023-11-15 00:34:13 -0600

    styed/implemented email validation

--- newsletter-sign-up-app/src/App.jsx
+++ newsletter-sign-up-app/src/App.jsx
@@ -3,8 +3,27 @@ import './App.css'
 
 function App() {
 
+  const [email, setEmail] = useState('');
+  const [good, setGood] = useState('');
+  const [valid, setValid] = useState('valid');
+  const [inputValid, setInputValid] = useState('');
+  const regex =  /^[a-zA-Z0-9+_.-]+@[a-zA-Z+-]+(\.[a-zA-z0-9]+)+$/g
+
+  const handleEmailVerification = () => {
+   
+    if (email.match(regex)) {
+      setGood('success')
+      setValid('valid')
+      setInputValid('')
+    } else {
+      setGood('')
+      setValid('invalid')
+      setInputValid('invalid-email-input')
+    }
+  }
+
   return (
-    <div className="container">
+    <div className="container ">
       <div className='app-container'>
         <main className='left'>
           <div className='left-content'>
@@ -15,11 +34,15 @@ function App() {
               <li>Measuring to ensure updates are a success</li>
               <li>And much more!</li>
             </ul>
-            <form onSubmit={(e) => e.preventDefault()}>
-              <h4>Email address</h4>
-              <input type="email" placeholder='email@company.com' />
+            <form onSubmit={(e) => e.preventDefault()} noValidate>
+              <div className='email-label'>
+                <h4>Email address</h4>
+                <h4 className={valid} id="invalid-email">Valid email required</h4>
+              </div>
+              <h2>{good} {valid}</h2>
+              <input onChange={(e) => setEmail(e.target.value)} type="email" placeholder='email@company.com' id={inputValid} />
               <br />
-              <button>Subscribe to monthly newsletter</button>
+              <button onClick={handleEmailVerification}>Subscribe to monthly newsletter</button>
             </form>
           </div>
         </main>
