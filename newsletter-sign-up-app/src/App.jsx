1119ea8b26f6a45b82a71aed2298faa59d84a845
Tevin Deale <103233487+TevinDeale@users.noreply.github.com>
2023-11-15 01:51:05 -0600

    created logic for popup and reload

--- newsletter-sign-up-app/src/App.jsx
+++ newsletter-sign-up-app/src/App.jsx
@@ -7,25 +7,37 @@ function App() {
   const [good, setGood] = useState('');
   const [valid, setValid] = useState('valid');
   const [inputValid, setInputValid] = useState('');
+  const [popup, setPopup] = useState(' success');
   const regex =  /^[a-zA-Z0-9+_.-]+@[a-zA-Z+-]+(\.[a-zA-z0-9]+)+$/g
 
   const handleEmailVerification = () => {
    
     if (email.match(regex)) {
-      setGood('success')
+      setGood(' success')
       setValid('valid')
       setInputValid('')
+      setPopup('')
     } else {
       setGood('')
       setValid('invalid')
       setInputValid('invalid-email-input')
+      setPopup(' success')
     }
   }
 
   return (
     <div className="container ">
       <div className='app-container'>
-        <main className='left'>
+        <main className={`success-popup${popup}`}>
+          <section>
+            <img src='../src/assets/images/icon-success.svg' alt='checkmark' />
+            <h1>Thanks for subscribing!</h1>
+            <p>A confirmation email has been sent to <strong>{email}</strong>. Please Open it and
+            click the button inside to confirm your subscription. </p>
+            <p onClick={() => {window.location.reload()}} id='button'>Dismiss message</p>
+          </section>
+        </main>
+        <main className={`left${good}`}>
           <div className='left-content'>
             <h1>Stay updated!</h1>
             <p>Join 60,000+ product managers receiving monthly updates on:</p>
@@ -39,14 +51,13 @@ function App() {
                 <h4>Email address</h4>
                 <h4 className={valid} id="invalid-email">Valid email required</h4>
               </div>
-              <h2>{good} {valid}</h2>
               <input onChange={(e) => setEmail(e.target.value)} type="email" placeholder='email@company.com' id={inputValid} />
               <br />
               <button onClick={handleEmailVerification}>Subscribe to monthly newsletter</button>
             </form>
           </div>
         </main>
-        <main className='right'>
+        <main className={`right${good}`}>
           <img src='../src/assets/images/illustration-sign-up-desktop.svg' alt='illustration' />
         </main>
       </div>
